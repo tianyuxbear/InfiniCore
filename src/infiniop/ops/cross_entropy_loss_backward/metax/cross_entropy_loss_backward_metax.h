07170495c06eb31b11af322fceb1ba860743d6b5
tianyuxbear <tianyuxbear@gmail.com>
2025-08-08 13:23:52 +0800

    dev: complete the migration of the operator to the MetaX platform

---
+++ src/infiniop/ops/cross_entropy_loss_backward/metax/cross_entropy_loss_backward_metax.h
@@ -0,0 +1,8 @@
+#ifndef __CROSS_ENTROPY_LOSS_BACKWARD_METAX_API_H__
+#define __CROSS_ENTROPY_LOSS_BACKWARD_METAX_API_H__
+
+#include "../../../elementwise/metax/elementwise_metax_api.h"
+
+ELEMENTWISE_DESCRIPTOR(cross_entropy_loss_backward, metax)
+
+#endif // __CROSS_ENTROPY_LOSS_BACKWARD_METAX_API_H__
